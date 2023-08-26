6654a8e430a5ceda7aad9a5b7b368feb06537fb0
Maksym Suslov <maxsusmail@gmail.com>
2023-08-26 21:42:36 +0000

    Global vs Local Components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <!-- With PascalCase you can use it as a self-closing tag -->
+    <!-- <TheHeader /> -->
+    <!-- Modern JS will automatically translate TheHeader into the-header -->
     <the-header></the-header>
     <badge-list></badge-list>
     <user-info
@@ -11,16 +14,25 @@
 </template>
 
 <script>
+// Instead of importing and registering components globally in main.js, you can import and register them locally in the component where you are going to use them
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
 export default {
+  components: {
+    TheHeader,
+    BadgeList,
+    UserInfo
+  },
   data() {
     return {
       activeUser: {
-        name: "Maximilian SchwarzmÃžller",
+        name: "Max Suslov",
         description: "Site owner and admin",
-        role: "admin",
-      },
+        role: "admin"
+      }
     };
-  },
+  }
 };
 </script>
 
